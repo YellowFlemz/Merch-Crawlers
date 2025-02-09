78bbc8c549619f6d4c8061584869c4029fd53f18
Shawn Lim <slim0154@student.monash.edu>
2025-02-09 17:45:20 +1100

    pagination added

--- animeworks-scraper/scraper/spiders/animeworks.py
+++ animeworks-scraper/scraper/spiders/animeworks.py
@@ -8,6 +8,7 @@ class AnimeworksSpider(scrapy.Spider):
     start_urls = ["https://animeworks.com.au/collections/pre-orders"]
 
     def parse(self, response):
+        # Scrapes product names and prices
         names = response.css("a.full-unstyled-link::text").getall()
         prices = response.css("span.price-item--regular::text").getall()
         for i, _ in enumerate(names):
@@ -15,3 +16,9 @@ class AnimeworksSpider(scrapy.Spider):
             item["name"] = names[i].strip()
             item["price"] = prices[i].strip()
             yield item
+
+        # Pagination
+        next_page = response.css('a.pagination__item-arrow[aria-label="Next page"]::attr(href)').get()
+        if next_page:
+            next_page_url = response.urljoin(next_page)
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
